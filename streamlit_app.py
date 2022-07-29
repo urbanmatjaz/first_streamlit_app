a051815bc9e1ee35c069e95ca5787486b2385867
urbanmatjaz <102015241+urbanmatjaz@users.noreply.github.com>
2022-07-29 10:28:31 +0200

    Popravljeni tabulatorji
    Tabulatorjev ne sme biti (vsaj za tele ukaze do zdaj)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit
 
 streamlit.title('My parents healthy diner')
-  streamlit.header('Breakfast menu')
-    streamlit.text('🥣 Omega 3 & bluberry oatmeal')
-    streamlit.text('🥗 kale, spinach & rocket smoothie')
-    streamlit.text('🐔 Hard boiled free range egg')
-    streamlit.text('🥑🍞 Avocado toast')
+streamlit.header('Breakfast menu')
+streamlit.text('🥣 Omega 3 & bluberry oatmeal')
+streamlit.text('🥗 kale, spinach & rocket smoothie')
+streamlit.text('🐔 Hard boiled free range egg')
+streamlit.text('🥑🍞 Avocado toast')
 
 
