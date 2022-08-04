1dc8cf21e82f03e770e50018f71fe18452667af6
urbanmatjaz <102015241+urbanmatjaz@users.noreply.github.com>
2022-08-04 10:13:13 +0200

    prestavljen response na dno strani

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
 
 streamlit.title('My parents healthy diner')
 
@@ -24,3 +24,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+
+streamlit.text(fruityvice_response)
