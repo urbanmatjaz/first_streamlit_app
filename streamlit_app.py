b4c98c2c32e09cda0222a4402bbe9fb50dec4a76
urbanmatjaz <102015241+urbanmatjaz@users.noreply.github.com>
2022-08-04 10:17:26 +0200

    Added json

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,5 +25,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
-streamlit.text(fruityvice_response)
+# New section to display Fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
