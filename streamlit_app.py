39737160c3c81573d9f515c6a39ad1f063ada09f
urbanmatjaz <102015241+urbanmatjaz@users.noreply.github.com>
2022-08-04 10:23:52 +0200

    dodan search field

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,8 @@ streamlit.dataframe(fruits_to_show)
 # New section to display Fruityvice api response
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
